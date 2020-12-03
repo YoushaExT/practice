01a396b81d1bbc29b1cc29c91a9134c73020833f
Youshaext <yousharunescape@yahoo.com>
2020-12-03 23:17:37 +0500

    the function now takes user promt as string input

--- cash/cash.py
+++ cash/cash.py
@@ -1,16 +1,16 @@
 # this program will calculate the min no. of coins req. to give change
 def main():
-    t=get_float()
+    t=get_float("Enter a float: ")
 
     print(t)
     print(type(t))
 
 
 
-def get_float():
+def get_float(s):
     while True:
         try:
-            test = float(input("Enter a float: "))
+            test = float(input(s))
             # if user entered a number, return will break the while loop
             return test
         except:
