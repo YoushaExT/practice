df610bd7a60084821ba9752d8326d47f0a4900f9
Youshaext <yousharunescape@yahoo.com>
2020-12-03 23:39:13 +0500

    add algo for calculating change, code complete

--- cash/cash.py
+++ cash/cash.py
@@ -1,9 +1,19 @@
 # this program will calculate the min no. of coins req. to give change
 def main():
-    t=get_pos_float("Enter a float: ")
+    cash=get_pos_float("Enter a float: ")
 
-    print(t)
-    print(type(t))
+    cash *= 100
+    # convert to int to get the floor value, round down
+    quarters = int(cash / 25)
+    quarters_rem = cash % 25
+    dimes = int(quarters_rem / 10)
+    dimes_rem = quarters_rem % 10
+    nickels = int(dimes_rem / 5)
+    pennies = int(dimes_rem % 5)
+
+    total = quarters+dimes+nickels+pennies
+
+    print(total)
 
 
 # ensures a positive float input
@@ -11,7 +21,7 @@ def get_pos_float(s):
     while True:
         flt=get_float(s)
         if flt>=0:
-            return flt            
+            return flt
         print("Not a positive float")
 
 def get_float(s):
