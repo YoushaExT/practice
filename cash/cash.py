31192792dc43ab10910d6006b907fc7bb4f39680
Youshaext <yousharunescape@yahoo.com>
2020-12-03 23:25:31 +0500

    add get_pos_float function

--- cash/cash.py
+++ cash/cash.py
@@ -1,11 +1,18 @@
 # this program will calculate the min no. of coins req. to give change
 def main():
-    t=get_float("Enter a float: ")
+    t=get_pos_float("Enter a float: ")
 
     print(t)
     print(type(t))
 
 
+# ensures a positive float input
+def get_pos_float(s):
+    while True:
+        flt=get_float(s)
+        if flt>=0:
+            return flt            
+        print("Not a positive float")
 
 def get_float(s):
     while True:
