142a03781555eedba1c2bc3763cfee8e9ae97d47
Youshaext <yousharunescape@yahoo.com>
2020-12-06 00:31:17 +0500

    alternative method: int(str,base)

--- moreproblems/q11.py
+++ moreproblems/q11.py
@@ -3,18 +3,22 @@
 #  Example: 0100,0011,1010,1001 Then the output should be: 1010 Notes: Assume the data is input by console.
 
 # create a function to create binary to decimal
-def bin_to_dec(n):
-    i=0
-    d=0
-    while True:
-        if n==0:
-            break
-        else:
-            d+=n%10*pow(2,i)
-            n=int(n/10)
-            i+=1
-    return d
+# def bin_to_dec(n):
+#     i=0
+#     d=0
+#     while True:
+#         if n==0:
+#             break
+#         else:
+#             d+=n%10*pow(2,i)
+#             n=int(n/10)
+#             i+=1
+#     return d
+#
+# dnum = [x for x in input().split(',') if bin_to_dec(int(x))%5==0]
+# print(','.join(dnum))
 
-dnum = [x for x in input().split(',') if bin_to_dec(int(x))%5==0]
+# alternatively use int(string,2) to convert directly
 
-print(','.join(dnum))
+l = [x for x in input().split(',') if int(x,2)%5==0]
+print(','.join(l))
