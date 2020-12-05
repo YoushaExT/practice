5a3b3bbab58cd591d56490f1cd59a8278ccb75f8
Youshaext <yousharunescape@yahoo.com>
2020-12-06 00:50:01 +0500

    q12.py, all even digited numbers in given range

---
+++ moreproblems/q12.py
@@ -0,0 +1,16 @@
+# Question: Write a program, which will find all such numbers between 1000 and 3000 (both included)
+# such that each digit of the number is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
+L=[]
+
+for i in range(1000,3001):
+    i=str(i)
+    # iterate through all 4 digits; len(i)=4
+    for j in range(len(i)):
+        # if not even dont check further
+        if int(i[j])%2!=0:
+            break
+        # if even and last iteration
+        if j==len(i)-1:
+            L.append(i)
+
+print(','.join(L))
