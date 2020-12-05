c2c7ad8126c885ed684bccc73108b4f23ee6597f
Youshaext <yousharunescape@yahoo.com>
2020-12-06 01:02:52 +0500

    q14.py calculate uppercase

---
+++ moreproblems/q14.py
@@ -0,0 +1,15 @@
+# Question: Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+# Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+
+s = input()
+
+uc=0
+lc=0
+
+for i in s:
+    if i.isupper():
+        uc+=1
+    elif i.islower():
+        lc+=1
+
+print(f"UPPER CASE {uc} LOWER CASE {lc}")
