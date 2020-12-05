38ba37d5bf85ac8ba65d63364cac42bff1b277c2
Youshaext <yousharunescape@yahoo.com>
2020-12-06 01:07:47 +0500

    q15.py, concatination and conversions

---
+++ moreproblems/q15.py
@@ -0,0 +1,8 @@
+# Question: Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+# Suppose the following input is supplied to the program: 9 Then, the output should be: 11106
+
+a=input()
+
+a=int(a)+int(2*a)+int(3*a)+int(4*a)
+
+print(a)
