b43516dc949a4a6f9ae9e930b531272f16b86e21
Youshaext <yousharunescape@yahoo.com>
2020-12-10 09:05:51 +0500

    q18.py password conditions

---
+++ moreproblems/q18.py
@@ -0,0 +1,49 @@
+# Question: A website requires the users to input username and password to register.
+#  Write a program to check the validity of password input by users. Following are the criteria for checking the password:
+#
+# At least 1 letter between [a-z]
+# At least 1 number between [0-9]
+# At least 1 letter between [A-Z]
+# At least 1 character from [$#@]
+# Minimum length of transaction password: 6
+# Maximum length of transaction password: 12
+# Your program should accept a sequence of comma separated passwords and will check them according to the above criteria.
+# Passwords that match the criteria are to be printed, each separated by a comma.
+# Example If the following passwords are given as input to the program:
+#  ABd1234@1,a F1#,2w3E*,2We3345 Then, the output of the program should be: ABd1234@1
+
+p = input()
+
+l = p.split(',')
+l2 =[]
+
+print(l)
+n=la=ua=c=ln=False
+# la = False
+# ua = False
+# c = False
+# ln = False
+
+for i in l:
+    for j in i:
+        if j.isnumeric():
+            print(f"{i} is numeric")
+            n = True
+        elif j.isalpha():
+            if j.lower() == j:
+                print(f"{i} is lower")
+                la = True
+            elif j.upper() == j:
+                print(f"{i} is upper")
+                ua = True
+        elif j in '$#@':
+            print(f"{i} is in $#@")
+            c = True
+    if len(i) >= 6 and len(i) <= 12:
+        print(f"{i} is 6-12")
+        ln = True
+    if n and la and ua and c and ln:
+        l2.append(i)
+    n=la=ua=c=ln=False
+
+print('.'.join(l2))
