ac26fc32176fb492083d63e70ef0806966168b4f
Youshaext <yousharunescape@yahoo.com>
2020-12-10 22:48:53 +0500

    q18.py, used re library for string search

--- moreproblems/q18.py
+++ moreproblems/q18.py
@@ -11,30 +11,29 @@
 # Passwords that match the criteria are to be printed, each separated by a comma.
 # Example If the following passwords are given as input to the program:
 #  ABd1234@1,a F1#,2w3E*,2We3345 Then, the output of the program should be: ABd1234@1
-
+import re
 p = input()
 
 l = p.split(',')
 l2 =[]
 
-n=la=ua=c=ln=False
-
-
 for i in l:
-    for j in i:
-        if j.isnumeric():
-            n = True
-        elif j.isalpha():
-            if j.islower():
-                la = True
-            elif j.isupper():
-                ua = True
-        elif j in '$#@':
-            c = True
-    if len(i) >= 6 and len(i) <= 12:
-        ln = True
-    if n and la and ua and c and ln:
-        l2.append(i)
-    n=la=ua=c=ln=False
+    if len(i) < 6 or len(i) > 12:
+        continue
+    # if no uppercase is found skip to the next string
+    if not re.search('[A-Z]',i):
+        continue
+    # if no lowercase is found skip to the next string
+    elif not re.search('[a-z]',i):
+        continue
+    # if no number is found skip to the next string
+    elif not re.search('[0-9]',i):
+        continue
+    # if no special character is found skip to the next string
+    elif not re.search('[$#@]',i):
+        continue
 
-print('.'.join(l2))
+    # all search checks passed, add to list
+    l2.append(i)
+
+print(','.join(l2))
