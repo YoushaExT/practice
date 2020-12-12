a6df350e3cf84a4906aa26e703273081cb999c95
Youshaext <yousharunescape@yahoo.com>
2020-12-12 08:02:59 +0500

    q50.py, using @staticmethod to define static methods in a class

---
+++ moreproblems/q50.py
@@ -0,0 +1,11 @@
+# Define a class named American which has a static method called printNationality.
+
+class American:
+    @staticmethod
+    def printNationality():
+        print("American")
+
+American1 = American()
+
+American1.printNationality()
+American.printNationality()
