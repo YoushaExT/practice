8443b3e0c64455df44ca0d325cf25566ea7e7dbc
Youshaext <yousharunescape@yahoo.com>
2020-12-12 06:34:24 +0500

    q44.py, easy, if str matches

---
+++ moreproblems/q44.py
@@ -0,0 +1,8 @@
+# Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or "Yes", otherwise print "No".
+
+s = input()
+
+if s=="YES" or s=="Yes" or s=="yes":
+    print("Yes")
+else:
+    print("No")
