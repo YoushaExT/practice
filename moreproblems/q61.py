b7a5dd56c7de2be06a9a0f24baddc7b9fc7403aa
Youshaext <yousharunescape@yahoo.com>
2020-12-13 09:58:05 +0500

    q61.py, unicode string, copied code (not that useful for python 3)

---
+++ moreproblems/q61.py
@@ -0,0 +1,8 @@
+# Print a unicode string "hello world"
+
+unicodeString = u"hello world!"
+print(unicodeString)
+
+# In Python 3, all strings are sequences of Unicode characters. There is a bytes type that holds raw bytes.
+#
+# In Python 2, a string may be of type str or of type unicode.
