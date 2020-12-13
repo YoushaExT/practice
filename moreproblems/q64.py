fc2c4425b0e59340c3c99254fb290e8b8538548d
Youshaext <yousharunescape@yahoo.com>
2020-12-13 10:04:05 +0500

    q64.py,easy, sum of fractions

---
+++ moreproblems/q64.py
@@ -0,0 +1,17 @@
+# Write a program to compute 1/2+2/3+3/4+...+n/n+1 with a given n input by console (n>0).
+#
+# Example: If the following n is given as input to the program:
+#
+# 5
+#
+# Then, the output of the program should be:
+#
+# 3.55
+
+n = int(input())
+
+sum=0
+for i in range(1,n+1):
+    sum+=i/(i+1)
+
+print(round(sum,2))
