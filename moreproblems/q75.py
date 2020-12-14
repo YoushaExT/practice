6b78fcef8e4bcc2e0c892e5031ef9aa36c6bb499
Youshaext <yousharunescape@yahoo.com>
2020-12-14 09:31:48 +0500

    q75.py, q74 but 5-95

---
+++ moreproblems/q75.py
@@ -0,0 +1,8 @@
+# Please generate a random float where the value is between 5 and 95 using Python math module.
+#
+# Hints: Use random.random() to generate a random float in [0,1].
+
+import random
+
+f=90*random.random()+5
+print(f)
