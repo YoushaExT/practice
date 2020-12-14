cc36c5418720ab1afb2100d43dc71766114e8919
Youshaext <yousharunescape@yahoo.com>
2020-12-14 09:36:20 +0500

    q76.py, random.choice to output randomly from a list

---
+++ moreproblems/q76.py
@@ -0,0 +1,9 @@
+# Please write a program to output a random even number between 0 and 10 inclusive using random module and list comprehension.
+#
+# Hints: Use random.choice() to a random element from a list.
+
+import random
+
+result = random.choice([x for x in range(11) if x%2==0])
+
+print(result)
