434da39d9110e8e70ea7cd2388774c31d27853ed
Youshaext <yousharunescape@yahoo.com>
2020-12-14 09:30:08 +0500

    q74.py, generate a random float between 10-100 (used random library random() func)

---
+++ moreproblems/q74.py
@@ -0,0 +1,8 @@
+# Please generate a random float where the value is between 10 and 100 using Python math module.
+#
+# Hints: Use random.random() to generate a random float in [0,1].
+
+import random
+
+f=90*random.random()+10
+print(f)
