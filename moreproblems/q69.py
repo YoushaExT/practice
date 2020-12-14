b20aa7b0c20907ff72acf796ee13228538c225e2
Youshaext <yousharunescape@yahoo.com>
2020-12-14 06:55:15 +0500

    q69.py, q68 but divisible by 5 and 7

---
+++ moreproblems/q69.py
@@ -0,0 +1,23 @@
+# Please write a program using generator to print the numbers
+# which can be divisible by 5 and 7 between 0 and n in comma separated form while n is input by console.
+#
+# Example: If the following n is given as input to the program:
+#
+# 100
+#
+# Then, the output of the program should be:
+#
+# 0,35,70
+
+def genFiveSeven(n):
+    for i in range(n+1):
+        if i%5==0 and i%7==0:
+            yield i
+
+n = int(input())
+
+L=[]
+for i in genFiveSeven(n):
+    L.append(str(i))
+
+print(','.join(L))
