739994e58759217b105738118b99c6170ec003bc
Youshaext <yousharunescape@yahoo.com>
2020-12-14 07:01:14 +0500

    q70.py, assert statement, (assert <condition>,<commentOptional>)

---
+++ moreproblems/q70.py
@@ -0,0 +1,8 @@
+# Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+#
+# Hints: Use "assert expression" to make assertion.
+
+L = [2,4,6,8]
+
+for i in L:
+    assert i%2==0,"not even"
