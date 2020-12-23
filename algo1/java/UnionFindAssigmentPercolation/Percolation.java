e6aef602a37e4473ff977fdae54bfa2aaac551d6
Youshaext <yousharunescape@yahoo.com>
2020-12-23 13:54:39 +0500

    fix backswing by adding another qu object,100% correct 100% timing, still failed 1 memory test

--- algo1/java/UnionFindAssigmentPercolation/Percolation.java
+++ algo1/java/UnionFindAssigmentPercolation/Percolation.java
@@ -14,6 +14,8 @@ public class Percolation {
     private final int n;
     private int count = 0;
     // private boolean percolates;
+    // todo - backwash
+    private final WeightedQuickUnionUF qu2;
 
     public Percolation(int n) {
         if (n < 1) throw new IllegalArgumentException(
@@ -23,24 +25,28 @@ public class Percolation {
         this.full = new boolean[n][n];
         // this.percolates = false;
         this.qu = new WeightedQuickUnionUF((n) * (n));
+        // todo - backwash - new UF object
+        this.qu2 = new WeightedQuickUnionUF((n) * n);
+
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < n; c++) {
                 grid[r][c] = false;
             }
         }
 
+        if (n > 1) {
+            //connecting all the first row elements
+            for (int c = 0; c < n; c++) {
+                qu.union(0, c);
+                qu2.union(0, c); // todo - backwash - connect first row but not the last row
+            }
 
-        //connecting all the first row elements
-        for (int c = 0; c < n; c++) {
-            qu.union(0, c);
+            //connecting all the last row elements
+            int index = n * (n - 1);
+            for (int c = 0; c < n; c++) {
+                qu.union(index, index + c);
+            }
         }
-
-        //connecting all the last row elements
-        int index = n * (n - 1);
-        for (int c = 0; c < n; c++) {
-            qu.union(index, index + c);
-        }
-
 
     }
 
@@ -53,7 +59,7 @@ public class Percolation {
         if (this.isOpen(row, col)) {
             return;
         }
-        //todo - first row? also full
+        // first row? also full
         if (row == 1) this.full[row - 1][col - 1] = true;
         // if not open; open it
         this.grid[row - 1][col - 1] = true;
@@ -64,32 +70,6 @@ public class Percolation {
         //  perform union with all neighbours
         int index = (row - 1) * (this.n) + col - 1;
 
-        // // up
-        // neighbours(index, row - 1, col);
-        // if (isFull(corner(row - 1), col)) {
-        //     this.full[row - 1][col - 1] = true; //turn both self and neighbours blue(full)
-        // }
-        // // down
-        // neighbours(index, row + 1, col);
-        // if (isFull(corner(row + 1), col)) {
-        //     this.full[row - 1][col - 1] = true;
-        // }
-        // // left
-        // neighbours(index, row, col - 1);
-        // if (isFull(row, corner(col - 1))) {
-        //     this.full[row - 1][col - 1] = true;
-        // }
-        // // right
-        // neighbours(index, row, col + 1);
-        // if (isFull(row, corner(col + 1))) {
-        //     this.full[row - 1][col - 1] = true;
-        // }
-        // if (isFull(row, col)) { //if i am full all my open neighbours become full
-        //     this.full[cornerZ(row - 2)][col - 1] = true && this.grid[cornerZ(row - 2)][col - 1];
-        //     this.full[cornerZ(row)][col - 1] = true && this.grid[cornerZ(row)][col - 1];
-        //     this.full[row - 1][cornerZ(col - 2)] = true && this.grid[row - 1][cornerZ(col - 2)];
-        //     this.full[row - 1][cornerZ(col)] = true && this.grid[row - 1][cornerZ(col)];
-        // }
 
         neighbours(index, row - 1, col);
         neighbours(index, row + 1, col);
@@ -113,16 +93,6 @@ public class Percolation {
         validate(row);
         validate(col);
 
-        // int index = (row - 1) * (this.n) + col - 1;
-
-        // if (this.full[row - 1][col - 1]) { //if i am full all my open neighbours become full
-        //     this.full[cornerZ(row - 2)][col - 1] = true && this.grid[cornerZ(row - 2)][col - 1];
-        //     this.full[cornerZ(row)][col - 1] = true && this.grid[cornerZ(row)][col - 1];
-        //     this.full[row - 1][cornerZ(col - 2)] = true && this.grid[row - 1][cornerZ(col - 2)];
-        //     this.full[row - 1][cornerZ(col)] = true && this.grid[row - 1][cornerZ(col)];
-        // }
-
-        // return this.full[row - 1][col - 1];
 
         if (!this.isOpen(row, col)) {
             return false; // if not open can't be full
@@ -143,7 +113,13 @@ public class Percolation {
         //         return true; // if connected to the first row
         //     }
         // }
-        if (qu.find(index) == qu.find(0)) {
+        // if (qu.find(index) == qu.find(0)) {
+        //     this.full[row - 1][col - 1] = true;
+        //     // if (row == this.n) this.percolates = true;
+        //     return true; // if connected to the first row
+        // }
+        // todo - backwash - qu2 instead of qu
+        if (qu2.find(index) == qu2.find(0)) {
             this.full[row - 1][col - 1] = true;
             // if (row == this.n) this.percolates = true;
             return true; // if connected to the first row
@@ -162,6 +138,8 @@ public class Percolation {
     public boolean percolates() {
         // if any of the last row is full it does
         int index = (this.n - 1) * (this.n);
+        // todo - fix for n=1
+        if (this.n == 1 && this.grid[0][0] == false) return false;
         if (qu.find(index) == qu.find(0))
             return true; // whole first and last rows are virtually connected so their first elements will immediately connect when any connection is made
         // for (int i = 1; i <= this.n; i++) {
@@ -189,19 +167,17 @@ public class Percolation {
 
         int index1 = (r - 1) * (this.n) + c - 1; // since qu is 0 indexed and single argument
         if (isOpen(r, c)) qu.union(index, index1);
+        // todo - backwash - qu2 aswell ; not removing the qu because those connections are used by percolation()
+        if (isOpen(r, c)) qu2.union(index, index1);
     }
 
+    // this will ensure that non-existing neighbours of corners are not referenced (they have values 0 and n+1 in 1 indexed arrays)
     private int corner(int rc) {
         if (rc == 0) rc = 1;
         if (rc > this.n) rc = this.n;
         return rc;
     }
 
-    private int cornerZ(int rc) { // for zero indexed
-        if (rc == -1) rc = 0;
-        if (rc >= this.n) rc = this.n - 1;
-        return rc;
-    }
 
     // test client (optional)
     public static void main(String[] args) {
